d1c64c69a83aa116073f95c1d36fded535c45dae
kwonhyeonju <rnjsguswn789@naver.com>
2019-03-08 06:51:44 -0800

    sum.h sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,11 +1,10 @@
 #include "sum.h"
 
-int sum(int n){
-	int res=0;
-	int i;
-	for(i=0; i<=n; i++){
-		res +=i;
+int sum(int a,b){
+	int hap=0;
+	for(i=0; a<=b; i++){
+	a += i;
+	hap += a;
 	}
-	return res;
 }
 
--- sum.h
+++ sum.h
@@ -1,4 +1,4 @@
 #pragma once
 
-int sum(int n);
+int sum(int a,b);
 
