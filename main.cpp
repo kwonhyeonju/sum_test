49abf61c4622c7498f3b8af2e0df00189eb20d38
kwonhyeonju <rnjsguswn789@naver.com>
2019-03-09 06:35:06 -0800

    modifed

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(){
 	int a,b;
-	scanf("%d %d",a,b);
+	scanf("%d %d",&a,&b);
 	int i = sum(a,b);
-	printf("%d", i);
+	printf("%d\n", i);
 }
 
--- sum.cpp
+++ sum.cpp
@@ -2,9 +2,10 @@
 
 int sum(int a,int b){
 	int hap=0;
-	for(int i=0; a<=b; i++){
-	a += i;
-	hap += a;
+	for(int i=0; i=b-a; i++){
+	int c = a+i;
+	hap += c;
 	}
+	return hap;
 }
 
