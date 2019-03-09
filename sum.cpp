6bd0758390262656e5ab376f9ca02d3a94f36778
kwonhyeonju <rnjsguswn789@naver.com>
2019-03-09 07:53:35 -0800

    modified sum.h sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,10 @@
 #include "sum.h"
 
-int sum(int a,int b){
-	int hap=0;
-	for(int i=0; i=b-a; i++){
-	int c = a+i;
-	hap += c;
+int sum(int a, int b){
+	int hap = 0;
+	for(int i=0; i<=b-a; i++){
+		int exe = a+i;
+		hap += exe;
 	}
 	return hap;
 }
